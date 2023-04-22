7ad984c083c7cd6b92bbce1937148a59f26699fb
Jackson <Jackson.McDowell@gmail.com>
2023-04-22 14:29:51 -0600

    main.c

--- RayTracing/main.c
+++ RayTracing/main.c
@@ -8,33 +8,24 @@ int main()
 {
     const int width = 256;
     const int height = 256;
-    char *fileName = "test.ppm";
-    const uint8_t rgbValue = 255;
 
-    struct Pixel frame[width][height];
+    newPPMFile(width, height, "test.ppm", 255);
 
-    for (int j = 0; j < height; j++)
+    for (int j = height - 1; j >= 0; j--)
     {
-        printf("Scanlines remaining: %d\n", (height - j));
-
         for (int i = 0; i < width; i++)
         {
             float r = (float)i / (float)width;
             float g = (float)j / (float)height;
             float b = 0.2f;
 
-            uint8_t ir = (int)(255.99f * r);
-            uint8_t ig = (int)(255.99f * g);
-            uint8_t ib = (int)(255.99f * b);
+            int ir = (int)(255.99f * r);
+            int ig = (int)(255.99f * g);
+            int ib = (int)(255.99f * b);
 
-            frame[height][width].red = ir;
-            frame[height][width].green = ig;
-            frame[height][width].blue = ib;
+            addPixeltoPPMFile(ir, ig, ib, "test.ppm");
         }
     }
 
-    writeToPPMFile(width, height, fileName, rgbValue, frame);
-
-    printf("Render complete");
     return 0;
 }
